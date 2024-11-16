2c19ea6fc37fcdab03b88f63c17c8ecf7894bd5f
AkhlakurRahman <akhlakur.rahmann@gmail.com>
2024-11-17 00:57:28 +0100

    feat. add: trusted companies, pricing section, and footer

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -1,6 +1,22 @@
 import { Button } from '@/components/ui/button';
 import { SignUpButton } from '@clerk/nextjs';
-import { ArrowRightIcon } from 'lucide-react';
+import { ArrowRightIcon, CheckIcon } from 'lucide-react';
+import Link from 'next/link';
+import { NeonIcon } from './_icons/Neon';
+import { ClerkIcon } from './_icons/Clerk';
+import { subscriptionTiersInOrder } from '@/data/subscriptionTiers';
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardFooter,
+	CardHeader,
+	CardTitle,
+} from '@/components/ui/card';
+import { formatCompactNumber } from '@/lib/numberFormatter';
+import { ReactNode } from 'react';
+import { cn } from '@/lib/utils';
+import { BrandLogo } from '@/components/BrandLogo';
 
 export default function HomePage() {
 	return (
@@ -21,6 +37,218 @@ export default function HomePage() {
 					</Button>
 				</SignUpButton>
 			</section>
+
+			<section className='bg-primary text-primary-foreground'>
+				<div className='container py-16 flex flex-col gap-16 px-8 md:px-16'>
+					<h2 className='text-3xl text-center text-balance'>
+						Trusted by top modern companies
+					</h2>
+
+					<div className='grid grid-cols-2 md:grid-cols-3 xl:grid-cols-5 gap-16'>
+						<Link href='https://neon.tech'>
+							<NeonIcon />
+						</Link>
+						<Link href='https://clerk.com'>
+							<ClerkIcon />
+						</Link>
+						<Link href='https://neon.tech'>
+							<NeonIcon />
+						</Link>
+						<Link href='https://clerk.com'>
+							<ClerkIcon />
+						</Link>
+						<Link href='https://neon.tech'>
+							<NeonIcon />
+						</Link>
+						<Link href='https://clerk.com'>
+							<ClerkIcon />
+						</Link>
+						<Link href='https://neon.tech'>
+							<NeonIcon />
+						</Link>
+						<Link href='https://clerk.com'>
+							<ClerkIcon />
+						</Link>
+						<Link href='https://neon.tech'>
+							<NeonIcon />
+						</Link>
+						<Link
+							href='https://clerk.com'
+							className='md:max-xl:hidden'
+						>
+							<ClerkIcon />
+						</Link>
+					</div>
+				</div>
+			</section>
+
+			<section id='pricing' className='px-8 py-16 bg-accent/5'>
+				<h2 className='text-4xl text-center text-balance font-semibold mb-8'>
+					Pricing software which pays for itself 20x over
+				</h2>
+
+				<div className='grid grid-cols-2 lg:grid-cols-4 gap-4 max-w-screen-xl mx-auto'>
+					{subscriptionTiersInOrder.map((tier) => (
+						<PricingCard key={tier.name} {...tier} />
+					))}
+				</div>
+			</section>
+
+			<footer className='container pt-16 pb-8 flex flex-col sm:flex-row gap-8 sm:gap-4 justify-between items-start'>
+				<Link href='/'>
+					<BrandLogo />
+				</Link>
+				<div className='flex flex-col sm:flex-row gap-8'>
+					<div className='flex flex-col gap-8'>
+						<FooterLinkGroup
+							title='Help'
+							links={[
+								{ label: 'PPP Discounts', href: '#' },
+								{ label: 'Discount API', href: '#' },
+							]}
+						/>
+						<FooterLinkGroup
+							title='Solutions'
+							links={[
+								{ label: 'Newsletter', href: '#' },
+								{ label: 'SaaS Business', href: '#' },
+								{ label: 'Online Courses', href: '#' },
+							]}
+						/>
+					</div>
+					<div className='flex flex-col gap-8'>
+						<FooterLinkGroup
+							title='Features'
+							links={[{ label: 'PPP Discounts', href: '#' }]}
+						/>
+						<FooterLinkGroup
+							title='Tools'
+							links={[
+								{ label: 'Salary Converter', href: '#' },
+								{ label: 'Coupon Generator', href: '#' },
+								{ label: 'Stripe App', href: '#' },
+							]}
+						/>
+						<FooterLinkGroup
+							title='Company'
+							links={[
+								{ label: 'Affiliate', href: '#' },
+								{ label: 'Twitter', href: '#' },
+								{ label: 'Terms of Service', href: '#' },
+							]}
+						/>
+					</div>
+					<div className='flex flex-col gap-8'>
+						<FooterLinkGroup
+							title='Integrations'
+							links={[
+								{ label: 'Lemon Squeezy', href: '#' },
+								{ label: 'Gumroad', href: '#' },
+								{ label: 'Stripe', href: '#' },
+								{ label: 'Chargebee', href: '#' },
+								{ label: 'Paddle', href: '#' },
+							]}
+						/>
+						<FooterLinkGroup
+							title='Tutorials'
+							links={[
+								{ label: 'Any Website', href: '#' },
+								{ label: 'Lemon Squeezy', href: '#' },
+								{ label: 'Gumroad', href: '#' },
+								{ label: 'Stripe', href: '#' },
+								{ label: 'Chargebee', href: '#' },
+								{ label: 'Paddle', href: '#' },
+							]}
+						/>
+					</div>
+				</div>
+			</footer>
 		</>
+	);
+}
+
+function PricingCard({
+	name,
+	priceInCents,
+	maxNumberOfProducts,
+	maxNumberOfVisits,
+	canAccessAnalytics,
+	canCustomizeBanner,
+	canRemoveBranding,
+}: (typeof subscriptionTiersInOrder)[number]) {
+	const isPopular = name === 'Standard';
+
+	return (
+		<Card>
+			<CardHeader>
+				<div className='text-accent font-semibold mb-8'>{name}</div>
+				<CardTitle className='text-xl font-semibold'>
+					${priceInCents / 100} /mo
+				</CardTitle>
+				<CardDescription>
+					{formatCompactNumber(maxNumberOfVisits)} pricing page visits
+					per month
+				</CardDescription>
+			</CardHeader>
+
+			<CardContent>
+				<SignUpButton>
+					<Button
+						variant={isPopular ? 'accent' : 'default'}
+						className='text-lg w-full rounded-lg'
+					>
+						Get Started
+					</Button>
+				</SignUpButton>
+			</CardContent>
+			<CardFooter className='flex flex-col gap-4 items-start text-sm'>
+				<Feature className='font-bold'>
+					{maxNumberOfProducts}{' '}
+					{maxNumberOfProducts === 1 ? 'product' : 'products'}
+				</Feature>
+				<Feature>PPP discounts</Feature>
+				{canAccessAnalytics && <Feature>Advanced analytics</Feature>}
+				{canRemoveBranding && (
+					<Feature>Remove easy PPP branding</Feature>
+				)}
+				{canCustomizeBanner && <Feature>Banner customization</Feature>}
+			</CardFooter>
+		</Card>
+	);
+}
+
+function Feature({
+	children,
+	className,
+}: {
+	children: ReactNode;
+	className?: string;
+}) {
+	return (
+		<div className={cn('flex items-center gap-2', className)}>
+			<CheckIcon className='size-4 stroke-accent bg-accent/25 rounded-full p-0.5' />
+			<span>{children}</span>
+		</div>
+	);
+}
+
+function FooterLinkGroup({
+	title,
+	links,
+}: {
+	title: string;
+	links: { label: string; href: string }[];
+}) {
+	return (
+		<div className='flex flex-col gap-4'>
+			<h3 className='font-semibold'>{title}</h3>
+			<ul className='flex flex-col gap-2 text-sm'>
+				{links.map((link) => (
+					<li key={link.label}>
+						<Link href={link.href}>{link.label}</Link>
+					</li>
+				))}
+			</ul>
+		</div>
 	);
 }
